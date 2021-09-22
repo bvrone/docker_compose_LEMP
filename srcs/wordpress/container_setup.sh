336f74a0c39e2c2766748bff487894b5c99c51af
Igor Kungurtsev <bvrone@student.21-school.ru>
2021-09-22 23:42:12 +0300

    wp-config with get_env is working

--- srcs/wordpress/container_setup.sh
+++ srcs/wordpress/container_setup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-if ! $(wp core is-installed); then
-    wp core install \
-        --allow-root    \
-        --url=${WP_SITE_URL}        \
-        --title=${WP_SITE_TITLE}    \
-        --admin_user=${DB_USER}     \
-        --admin_password=${DB_PASSWORD} \
-        --admin_email=${WP_ADMIN_EMAIL}
-fi
+# if ! $(wp core is-installed --allow-root); then
+#	wp config shuffle-salts --allow-root
+#     wp core install \
+#         --allow-root    \
+#         --url=${WP_SITE_URL}        \
+#         --title=${WP_SITE_TITLE}    \
+#         --admin_user=${DB_USER}     \
+#         --admin_password=${DB_PASSWORD} \
+#         --admin_email=${WP_ADMIN_EMAIL}
+# fi
 
 exec "$@"
+
+# wp core install --allow-root --url=${WP_SITE_URL} --title=${WP_SITE_TITLE} --admin_user=${DB_USER} --admin_password=${DB_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
+
+# wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST}
